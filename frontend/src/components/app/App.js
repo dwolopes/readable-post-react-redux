8991a71f4a3be81b09b2b58d6d1b29553bfe85fc
Douglas Lopes <dwillian.lopes@gmail.com>
2018-12-10 05:06:50 -0500

    feat: first commit with frontends structure added

---
+++ frontend/src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    );
+  }
+}
+
+export default App;
